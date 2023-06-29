62f2f27e5433f19950af05b74f03a99d2b4c5c75
vinay1357 <vinaykamra02@gmail.com>
2023-06-30 02:24:41 +0530

    Initial Commit

---
+++ Assignment_Calculator/Assignment_CalculatorApp.swift
@@ -0,0 +1,19 @@
+//
+//  Assignment_CalculatorApp.swift
+//  Assignment_Calculator
+//
+//  Created by VinayKamra on 27/06/23.
+//
+
+import SwiftUI
+import Calculator
+
+@main
+struct Assignment_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CalculatorView()
+        }
+    }
+}
+    
