b29a29a989aba98fc392e66d1d7350441468bd15
vinay1357 <vinaykamra02@gmail.com>
2023-07-04 14:48:42 +0530

    Added Crashlytics support

--- Assignment_Calculator/Assignment_CalculatorApp.swift
+++ Assignment_Calculator/Assignment_CalculatorApp.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 import Calculator
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct Assignment_CalculatorApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             CalculatorView()
